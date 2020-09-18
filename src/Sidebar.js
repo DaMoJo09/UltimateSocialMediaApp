a24015775832622a916e4a3a1eeee694b8dfc8d2
DaMoJo09 <62726601+DaMoJo09@users.noreply.github.com>
2020-09-18 12:48:48 -0700

    connected to DB

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,14 +1,27 @@
-import React from "react";
-import { Avatar, IconButton } from "@material-ui/core";
-import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import React, { useEffect, useState } from "react";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
-import SidebarChat from "./SidebarChat";
 import SearchOutlined from "@material-ui/icons/SearchOutlined";
-
 import "./Sidebar.css";
+import SidebarChat from "./SidebarChat";
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import db from "./firebase";
 
 function Sidebar() {
+  const [rooms, setRooms] = useState([]);
+
+  useEffect(() => {
+    db.collection("rooms").onSnapshot((snapshot) => {
+      setRooms(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      );
+    });
+  }, []);
+
   return (
     <div className="sidebar">
       <div className="sidebar__header">
@@ -25,22 +38,17 @@ function Sidebar() {
           </IconButton>
         </div>
       </div>
-
       <div className="sidebar__search">
         <div className="sidebar__searchContainer">
           <SearchOutlined />
           <input placeholder="Search / New Chat" type="text" />
         </div>
       </div>
-
       <div className="sidebar__chats">
         <SidebarChat addNewChat />
-        <SidebarChat />
-        <SidebarChat />
-        <SidebarChat />
-        <SidebarChat />
-        <SidebarChat />
-        <SidebarChat />
+        {rooms.map((room) => (
+          <SidebarChat key={room.id} id={room.id} name={room.data.name} />
+        ))}
       </div>
     </div>
   );
--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Avatar } from "@material-ui/core";
 import "./SidebarChat.css";
+import db from "./firebase";
 
-function SidebarChat({ addNewChat }) {
+function SidebarChat({ id, name, addNewChat }) {
   const [seed, setSeed] = useState("");
 
   useEffect(() => {
@@ -14,6 +15,9 @@ function SidebarChat({ addNewChat }) {
 
     if (roomName) {
       // database stuff...
+      db.collection("rooms").add({
+        name: roomName,
+      });
     }
   };
 
@@ -21,7 +25,7 @@ function SidebarChat({ addNewChat }) {
     <div className="sidebarChat">
       <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
       <div className="sidebarChat__info">
-        <h2> Room Name </h2>
+        <h2> {name} </h2>
         <p>last message...</p>
       </div>
     </div>
